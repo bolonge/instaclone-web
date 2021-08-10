7a0744d4588c05b854d993b5af98803b76dfa44a
bolonge <xxs456@naver.com>
2021-08-10 16:30:28 +0900

    apollo reactive variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { useReactiveVar } from '@apollo/client';
 import {HashRouter as Router} from 'react-router-dom'
+import { isLoggedInVar } from './apollo';
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar)
   return (
     <div>
       <Router>
-        <p>
-
-        </p>
+        
       </Router>
     </div>
   );
