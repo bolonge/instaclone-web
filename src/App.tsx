4e468845d772fb17bf376d8a0efb90d67393904f
bolonge <xxs456@naver.com>
2021-08-10 17:31:48 +0900

    styled setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useReactiveVar } from '@apollo/client';
-import {HashRouter as Router} from 'react-router-dom'
-import { isLoggedInVar } from './apollo';
+import { useReactiveVar } from "@apollo/client";
+import { HashRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar)
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <div>
-      <Router>
-        
-      </Router>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <div>
+        <Router></Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
