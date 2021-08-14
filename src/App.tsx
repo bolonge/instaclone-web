a1ccd45f7cb42a6d4e3ee82086f8d0a42c4f76fe
bolonge <xxs456@naver.com>
2021-08-14 22:19:43 +0900

    add auth components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useReactiveVar } from "@apollo/client";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { darkModeVar, isLoggedInVar } from "./apollo";
+import routes from "./routes";
 import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
@@ -11,11 +13,18 @@ function App() {
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
-      <div>
-        <Router>
-          <Login></Login>
-        </Router>
-      </div>
+      <Router>
+        <Switch>
+          <Route path={routes.home} exact></Route>
+          {!isLoggedIn ? (
+            <Route path={routes.signUp}>
+              <SignUp></SignUp>
+            </Route>
+          ) : null}
+          <Route></Route>
+        </Switch>
+        <Login></Login>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+interface RouteProp {
+    home: string
+    signUp: string
+}
+
+const routes: RouteProp = {
+    home: "/",
+    signUp: "/sign-up"
+}
+
+export default routes;
