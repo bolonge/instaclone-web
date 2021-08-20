ad33ee71a6f933d4b39570723c87b85414d469c0
bolonge <xxs456@naver.com>
2021-08-21 01:14:20 +0900

    sign up page

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -10,6 +10,21 @@ import BottomBox from "../components/auth/BottomBox";
 import routes from "../routes";
 import { FatLink } from "../components/shared";
 import PageTitle from "../components/PageTitle";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+import {
+  createAccount,
+  createAccountVariables,
+} from "../__generated__/createAccount";
+
+interface IForm {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  username: string;
+  password: string;
+}
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -22,7 +37,55 @@ const Subtitle = styled(FatLink)`
   text-align: center;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 const SignUp: React.FunctionComponent = () => {
+  const history = useHistory();
+  const [createAccount, { loading }] = useMutation<
+    createAccount,
+    createAccountVariables
+  >(CREATE_ACCOUNT_MUTATION, {
+    onCompleted: (data) => {
+      const { createAccount } = data;
+      if (!createAccount?.ok) {
+        return;
+      }
+      history.push(routes.home);
+    },
+  });
+  const { register, handleSubmit, formState, setError } = useForm<IForm>({
+    mode: "onChange",
+  });
+
+  const onSubmitValid: SubmitHandler<IForm> = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
   return (
     <AuthLayout>
       <PageTitle title="Sign up"></PageTitle>
@@ -33,12 +96,37 @@ const SignUp: React.FunctionComponent = () => {
             Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign Up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", { required: "First Name is required" })}
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("email", { required: "Email is required" })}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            {...register("username", { required: "User name is required" })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register("password", { required: "Password is required" })}
+            type="password"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox
